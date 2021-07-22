a3cad5688d3f0409517eaade4383b27b13d6fc16
pablomuro <pablo.muro@gmail.com>
2021-07-22 09:30:28 -0300

    feature/add sass support

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,23 +32,27 @@
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+
+  <div class="teste">
+    <div class="div">asaas</div>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue";
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    const count = ref(0);
+    return { count };
+  },
+});
 </script>
 
 <style scoped>
