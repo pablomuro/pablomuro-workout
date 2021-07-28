efe68af5d55a60f520c87fac023ed3ed46570c3c
pablomuro <pablo.muro@gmail.com>
2021-07-27 22:14:56 -0300

    feature/add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), VitePWA({
+    mode: 'development',
+    registerType: 'autoUpdate',
+  })]
 })
