431a54c2bac34ed70d43e35a39a15ea12a8f43d4
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2026-09-16 14:20:33 +0000

    add treino D and finisher

--- src/typings/workout.ts
+++ src/typings/workout.ts
@@ -1,20 +1,24 @@
-export interface Workout {
-  workout: string;
-  "muscule-group": string[];
-  exercises: Exercise[];
-  header?: string;
+export interface WorkoutJson {
+  workout: string
+  'muscule-group': string[]
+  exercises: Exercise[]
+  finisher?: string[]
+}
+
+export interface Workout extends WorkoutJson {
+  header?: string // TS map, not JSON
 }
 
 export interface Exercise {
-  exercise: string;
-  series: number;
-  reps: number | string;
-  done: boolean;
-  rest?: number;
+  exercise: string
+  series: number
+  reps: number | string
+  done: boolean
+  rest?: number
 }
 
 export interface JsonWorkoutType {
-  restTime: number;
-  abdominal?: any[];
-  workoutList: any;
+  restTime: number
+  abdominal?: any[]
+  workoutList: any
 }
